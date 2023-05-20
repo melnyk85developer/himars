ed334be016aacde1a06ced124af60a8a32b4e14d
Maksym <133916813+melnyk85developer@users.noreply.github.com>
2023-05-20 07:34:33 +0200

    add new src

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,46 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { create } from "react-test-renderer";
+import ProfileStatus from 'pages/Profile/Info/ProfileStatus';
+// import App from './components/App/App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("ProfileStatus component", () => {
+
+  test("after creation <span> should be displayed", () => {
+    const component = create(<ProfileStatus status="Hello HiMars" />);
+    const root = component.root;
+    let span = root.findAllByType("span");
+    expect(span).not.toBeNull();
+  });
+
+  test("input should be displayed editNode instead of span", () => {
+    const component = create(<ProfileStatus status="Hello HiMars" />);
+    const root = component.root;
+    let span = root.findByType("span");
+    span.props.onDoubleClick();
+    let input = root.findByType("input");
+    expect(input.props.value).toBe("Hello HiMars")
+  });
+
+  // test("status from props should be in the state", () => {
+  //   const component = create(<ProfileStatus status="Hello HiMars" />);
+  //   const root = component.root;
+  //   expect(root.state.status).toBe("HiMars.in");
+  // });
+
+  // test("after creation <input> shouldn't be displayed", () => {
+  //   const component = create(<ProfileStatus status="Hello HiMars" />);
+  //   const root = component.root;
+  //   expect( () => {
+  //       let input = root.findAllByType("input");
+  //   }).toThrow();
+  // });
+
+  // test("after creation <span> should contains correct status", () => {
+  //   const component = create(<ProfileStatus status="Hello HiMars" />);
+  //   const root = component.root;
+  //   let span = root.findAllByType("span");
+  //   expect(span.children[0]).toBe("Hello HiMars");
+  // });
+
 });
